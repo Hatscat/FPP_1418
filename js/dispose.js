e71d628e83efe9de2cbdc3d58c114f65c9215bbd
solfen <solfen@hotmail.com>
2014-03-25 12:02:35 +0100

    Fix dispose
    plus d'érreurs qui suppriment des choses

--- js/dispose.js
+++ js/dispose.js
@@ -1,21 +1,26 @@
 function disposeThings(config)
 {
-	config.ground.mesh.dispose(false);
+	config.ground.mesh.dispose(true);
 
 	for( var c in config.table)
 	{
-		config.table[c].dispose(false);
+		config.scene._toBeDisposed.push(config.table[c])
 	}
+	config.table = {};
 
 	for (var i = 0; i<config.villages.length;i++)
-		config.villages[i].mesh.dispose(false);
+		config.scene._toBeDisposed.push(config.villages[i].mesh)
+
+	config.villages = [];
 
 	for (var i = 0; i<config.arbres.length;i++)
 	{
 		for (b in config.arbres[i])
 		{
 			if(config.arbres[i][b])
-				config.arbres[i][b].dispose(false);
+				config.scene._toBeDisposed.push(config.arbres[i][b])
 		}
 	}
+
+	config.arbres = [];
 };
